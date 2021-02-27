92e9d9a553306baeff962e6820f427051d7f4606
Luke Davis <luke.ca.davis@gmail.com>
2021-02-26 18:39:00 -0800

    Renamed listing files

--- listing5-1b.py
+++ listing5-1b.py
@@ -3,7 +3,7 @@ planets = { "Mercury": "The smallest planet, nearest yo the Sun",
             "Earth": "The only planet known to have native life",
             "Mars": "The Red Planet is the second smallest planet",
             "Jupiter": "The largest planet, Jupiter is a gas giant",
-            "Satern": "the secound largest planet is a gas giant",
+            "Saturn": "the secound largest planet is a gas giant",
             "Uranus": "An ice giant with a ring system",
             "Neptune": "An ice giant and farthest frome the Sun"
             }
